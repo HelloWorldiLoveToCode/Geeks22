151f099cd510f1de2fa34888133ffcf1ff739eeb
Alexlejarraga02 <alexlejarragamorales@gmail.com>
2023-02-23 20:39:33 -0500

    Creation of controllers

---
+++ Geeks22/Geeks22WebApi/Controllers/DetailsController.cs
@@ -0,0 +1,31 @@
+﻿using Geeks22WebApi.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Geeks22WebApi.Models;
+using System.Collections;
+using System.Text;
+using System.Net.Http;
+using System.Net;
+using System.Security.Principal;
+using System.Web.UI.WebControls;
+using System.Web.Http.Results;
+
+namespace Geeks22WebApi.Controllers
+{
+    public class DetailsController : Controller
+    {
+
+        public MyDbContext DbContext { get; set; }
+        public DetailsController() => DbContext = new MyDbContext();
+
+     
+        
+        
+    }
+}
